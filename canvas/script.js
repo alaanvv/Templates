b7256f590f71a7f85573fe5e5e7865292eb17fac
Alan Vale <valealan84@gmail.com>
2023-11-16 19:19:12 -0300

    Update script.js
    added line func

--- canvas/script.js
+++ canvas/script.js
@@ -12,6 +12,24 @@ function dot(x, y, c) {
   ctx.fillRect(x, y, 1, 1)
 }
 
+function line(x1, y1, x2, y2, c) {
+  const x_size = Math.abs(x1 - x2) + 1
+  const y_size = Math.abs(y1 - y2) + 1
+  const x_step = Math.sign(x2 - x1)
+  const y_step = Math.sign(y2 - y1)
+
+  const x_per_y = x_size / y_size
+
+  for (let x = x1; x !== x1 + x_step * x_size; x += x_step) {
+    let y = Math.round(y1 + (x - x1) / x_per_y * y_step)
+    
+    for (let y_streak = 0; y_streak < 1 / x_per_y; y_streak++) {
+      dot(x, y, c)
+      y++
+    }
+  }
+}
+
 function clear() {
   ctx.fillStyle = 'black'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
